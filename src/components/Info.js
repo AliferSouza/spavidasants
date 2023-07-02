31d7cd15890180345400cbe0e98648cf9b99df1b
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-07-02 02:18:25 -0300

    Delete src directory

--- src/components/Info.js
+++
@@ -1,6 +0,0 @@
-export default async function Info(){
-     return `
-    <input type="text" oninput="document.getElementById('meuSpan').innerHTML = this.value;" value="">
-    <span id="meuSpan"></span>
-    `
-}
