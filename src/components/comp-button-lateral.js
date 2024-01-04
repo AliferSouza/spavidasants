373a6515eb49b8dd15de8d9c0a0a21cbf56583a1
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2024-01-04 20:52:47 -0300

    Update comp-button-lateral.js

--- src/components/comp-button-lateral.js
+++ src/components/comp-button-lateral.js
@@ -8,7 +8,7 @@ export default  function video({tag}) {
     } else {
       const videoHTML = `
         <video autoplay id="videoPalyPause" width="90%" height="210">
-          <source src="public/video/spa.mp4" type="video/mp4">
+          <source src="/video/spa.mp4" type="video/mp4">
         </video>
       `;
       videoComp.innerHTML = videoHTML;
