65e3a61c3d7688ca4f5034b3545d6d4e3f64fabb
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2024-01-18 13:34:09 -0300

    Add files via upload

--- src/pages/catalogo.js
+++ src/pages/catalogo.js
@@ -4,30 +4,31 @@ export default async function catalogos() {
 
     return `   
        <menu-principal></menu-principal>   
-         <div class="container_page">    
-            <div class="catalogo">            
-           
-                <div class="catalogo_card">         
+      
+            <div class="catalogo">          
+            <div class="catalogo_cards"> 
+                <div class="card">         
                 <div class="catalogo_card_interno">
                 <img src="/catalogos/01.jpeg" alt="" >
-                <span>Catálogo de tratamentos estéticos e faciais... <span>
-                <a href="/catalogos/01.pdf" download="Terapias.pdf">Download</a>
+                <div>Catálogos de tratamentos estéticos e faciais...
+                <a href="/catalogos/01.pdf" download="Terapias.pdf">Download</a> </div>
+               
                 </div>
                 </div>
 
      
-                <div class="catalogo_card">   
-                         
+                <div class="card">                       
                 <div class="catalogo_card_interno">
                 <img src="/catalogos/02.jpeg"  alt="" >
-                <span>Catálogo com nossos pacotes de SPA...<span>
-                <a href="/catalogos/02.pdf" download="Estetica.pdf">Download</a>
+                <div>Catálogos com nossos pacotes de SPA...
+                <a href="/catalogos/02.pdf" download="Estetica.pdf">Download</a></div>
+               
 
                 </div>
-                </div>
-              
+            </div>
+            </div>
             </div>                          
-        </div>
+    
       `;
   }
   
