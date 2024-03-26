a4e762313f5d4ad91537c0e66539c74a70058087
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2024-03-26 17:30:15 -0300

    Update acompanhar.js

--- src/pages/acompanhar.js
+++ src/pages/acompanhar.js
@@ -1,7 +1,10 @@
 export default async function pedido() {
+  
+  document.title = "AGENDAMENTOS";
   return`
   <menu-principal ></menu-principal>
   <div class="container_pedido">
     <comp-pedido></comp-pedido>       
   </div>`
 }
+
