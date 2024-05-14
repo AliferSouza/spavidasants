4469c1f6e9895c56b6194d3112dec19600800962
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2024-05-14 20:12:56 -0300

    Update comp-catalogo.js

--- src/components/comp-catalogo.js
+++ src/components/comp-catalogo.js
@@ -1,18 +1,22 @@
-export default function compCatalogo({tag}) {
+export default function compCatalogo() {
 
 
     return `
+
+    <div id="cat-cot">
 
     <div id="cat-01">   
     <h3>Catálogo de Promoções<span>&#8594;</span></h3>
     <span>Acreditamos que todos merecem um momento de relaxamento e rejuvenescimento.
      Nossas massagens são projetadas para proporcionar uma 
-    experiência indulgente que revitaliza tanto o corpo quanto a mente.</span>
+    experiência indulgente que revitaliza tanto o corpo quanto a mente. <h5 style="color: red">Clique na imagem &#8594;</h5></span>
     </div>
   
-    <div id="cat-02">   
+    
+    <a href="/catalogos/Promoçoes2024.pdf" download="Promoções.pdf">
     <img src="/img/promoções/promocao.jpeg">
-    <a href="/catalogos/Promoçoes2024.pdf" download="Promoções.pdf">Download</a>
+    </a>
+  
     </div>
 
     `
