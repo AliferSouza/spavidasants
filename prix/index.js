ffc9dd9f4b039d95b1261952966d8241c17399c6
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2024-06-05 17:27:23 -0300

    Delete prix directory

--- prix/index.js
+++
@@ -1,8 +0,0 @@
-import Router from "./front-end/ROUTER/index.js";
-import STATE_GLOBAL from "./front-end/STATE_GLOBAL/index.js";
-const macros = STATE_GLOBAL.macros;
-if (macros) {
-  Router();
-  Object.values(macros).forEach((func) => func());
-}
-
