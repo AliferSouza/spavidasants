30130972464665935c93612012a194032c4407a5
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2024-07-04 17:31:19 -0300

    Add files via upload

--- src/components/comp-slider.js
+++ src/components/comp-slider.js
@@ -19,7 +19,8 @@ export default function slider({$}) {
       <button class="prev" id="prevBtn">&#10094;</button>
       <img style="display: block" class="mySlides" src="/img/cursos/1.png" alt="Slider 1 Novidades">
       <img class="mySlides" src="/img/cursos/2.png" alt="Slider 1 Novidades">
-      <img class="mySlides" style=" max-width: 100%; height: auto;" src="/img/cursos/3.png" alt="Cursos">
+      <img class="mySlides" src="/img/cursos/3.png" alt="Slider 1 Novidades">
+      <img class="mySlides" style=" max-width: 100%; height: auto;" src="/img/cursos/cursos.jpeg" alt="Cursos">
       <button class="next" id="nextBtn">&#10095;</button>      
     </div>`;
 }
